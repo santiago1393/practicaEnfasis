251d65af3429ca3e954da0c59b15f0782bd5ff8a
Santiago Castaño <santi1393@hotmail.com>
2015-05-20 01:45:48 -0500

    Metodo DELETE actualizado.. Ya funciona en otros dominios..

--- practica/src/main/java/co/com/udem/practica/controller/MaquinasController.java
+++ practica/src/main/java/co/com/udem/practica/controller/MaquinasController.java
@@ -65,7 +65,11 @@ public class MaquinasController {
 	}
 	
 	@RequestMapping("/delete")
-	public @ResponseBody EstadoRespuesta MaquinaDelete(@RequestParam(value = "id", required = false, defaultValue = "0") Long id){
+	public @ResponseBody EstadoRespuesta MaquinaDelete(HttpServletResponse response,@RequestParam(value = "id", required = false, defaultValue = "0") Long id){
+		   response.setHeader("Access-Control-Allow-Origin", "*");
+           response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+           response.setHeader("Access-Control-Max-Age", "3600");
+           response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
 		return  maquinaService.metodoDelete(id);
 	
 	}
